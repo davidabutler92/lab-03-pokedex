d1386649a0fba511b6f6c3af7bd26e491964da73
davidabutler92 <davidabutler92@yahoo.com>
2020-10-29 20:14:26 -0700

    add comment out my throw error

--- src/services/pokemonApi.js
+++ src/services/pokemonApi.js
@@ -3,7 +3,7 @@ import request from 'superagent';
 export const getAllPokemon = () => {
     return request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex?page=1&perPage=1000')
         .then(res => {
-            if(!res.ok) throw 'Could not get pokemon';
+            // if(!res.ok) throw 'Could not get pokemon';
             return res.body;
         })
         .then(res => {
@@ -14,7 +14,7 @@ export const getAllPokemon = () => {
 export const getPokeonByName = pokemonName => {
     return request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${pokemonName}`)
         .then(res => {
-            if(!res.ok) throw 'Could not get pokemon by name'
+            // if(!res.ok) throw 'Could not get pokemon by name'
             return res.body;
         })
         .then(res => {
