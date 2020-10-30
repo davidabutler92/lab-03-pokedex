d4d52b9a40d9becd96e13b7b4f03cd4647e13409
davidabutler92 <davidabutler92@yahoo.com>
2020-10-30 10:56:18 -0700

    add styling to homePage

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './HomePage.css';
 import { Link } from 'react-router-dom';
+import pokeball from '../../assets/pokeball.png';
+
 
 export default function HomePage() {
     return (
@@ -8,10 +10,11 @@ export default function HomePage() {
         <section className='section'>
             <div className='homeDiv'>
                 <h1 className='title'>Pokédex</h1>
+                <img className='pokeball'src={pokeball} alt='pokeball'></img>
                 <Link className='link' to='/search'><button className='button'>Search</button></Link>
             </div>
             <div className='smallTitle'>
-                <p>First gen Pokémon</p>
+                <p>All Generations of Pokémon</p>
             </div>
         </section>
         </>
